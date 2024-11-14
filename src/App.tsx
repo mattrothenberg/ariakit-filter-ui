551b39ab200c4b483432e0d8f9e9c6250209e552
Matt Rothenberg <mattrothenberg@gmail.com>
2024-11-13 21:55:43 -0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { FilterMenu } from "./components/filter-menu";
 
 export default function App() {
 	return (
-		<div className="p-4">
-			<FilterMenu />
+		<div>
+			<a
+				target="_blank"
+				className="font-mono text-white text-sm bg-black p-3 hover:bg-black/90 block text-center"
+				href="https://github.com/mattrothenberg/ariakit-filter-ui"
+				rel="noreferrer"
+			>
+				mattrothenberg/ariakit-filter-ui
+			</a>
+			<div className="p-4">
+				<FilterMenu />
+			</div>
 		</div>
 	);
 }
